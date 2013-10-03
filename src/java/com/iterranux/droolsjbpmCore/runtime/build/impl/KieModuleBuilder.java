599d244937dc44175f3fd979e144b00d92299062
Alexander Herwix <alex@herwix.com>
2013-10-03 15:16:13 +0200

    Updated Copyright

--- src/java/com/iterranux/droolsjbpmCore/runtime/build/impl/KieModuleBuilder.java
+++ src/java/com/iterranux/droolsjbpmCore/runtime/build/impl/KieModuleBuilder.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2013. Alexander Herwix
+ * This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You can also obtain a commercial license. Contact: alex@herwix.com for further details.
+ */
+
 package com.iterranux.droolsjbpmCore.runtime.build.impl;
 
 import com.iterranux.droolsjbpmCore.internal.DroolsjbpmCoreUtils;
@@ -25,7 +42,7 @@ public class KieModuleBuilder {
 
     public static final String KMODULE_ROOT_PATH = "src/main/resources/";
 
-    public static final String KMODULE_GROUP_ID = "org.grails.plugins";
+    public static final String KMODULE_GROUP_ID = "org.grails.kieModules";
 
     private KieServices kieServices;
 
